6da2f9c2272467c4322b6b791154cd37d91b2d4b
Louis Kraemer <louisk@bam.tech>
2018-12-26 12:01:46 +0100

    feat(lottie): added lottie-react-native for android

--- android/app/src/main/java/com/smarthomeapp/MainApplication.java
+++ android/app/src/main/java/com/smarthomeapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.smarthomeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new RNGestureHandlerPackage()
       );
     }
