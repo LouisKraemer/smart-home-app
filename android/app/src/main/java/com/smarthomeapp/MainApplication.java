b1e076af9f47c65570c540d662e53fcd2bae3772
Louis Kraemer <louisk@bam.tech>
2019-03-26 20:46:10 +0100

    zhefkbdjezfijzlefo

--- android/app/src/main/java/com/smarthomeapp/MainApplication.java
+++ android/app/src/main/java/com/smarthomeapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.lottie.LottiePackage;
+import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new LottiePackage(),
+            new ReactSliderPackage(),
             new RNGestureHandlerPackage()
       );
     }
