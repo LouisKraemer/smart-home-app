2d8aa294cc2a2c00136ce42e99fc569c88de8121
Louis Kraemer <louisk@bam.tech>
2019-03-29 22:26:54 +0100

    chore(package.json): added asyncstorage

--- android/app/src/main/java/com/smarthomeapp/MainApplication.java
+++ android/app/src/main/java/com/smarthomeapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.smarthomeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new LottiePackage(),
             new ReactSliderPackage(),
             new RNGestureHandlerPackage()
