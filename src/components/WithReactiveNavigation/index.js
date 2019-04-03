42c2f3f446a6b2083f1bce776ae2d0bae64ab1b3
Louis Kraemer <louisk@bam.tech>
2019-04-03 09:15:53 +0200

    chore(navigation): removed reactive navigation

--- src/components/WithReactiveNavigation/index.js
+++
@@ -1,1 +0,0 @@
-export { withReactiveNavigation } from './WithReactiveNavigation';
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { withContainer } from './WithContainer';
 export { BulbItem } from './BulbItem';
 export { WSModal } from './WSModal';
-export { withReactiveNavigation } from './WithReactiveNavigation';
 export { Swicth } from './Switch';
+export { ColorPicker } from './ColorPicker';
